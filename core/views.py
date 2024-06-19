2833d17e5dcd380714c3dd7968c11fd5af47b7fb
Nycollas Henich <nhhenich@gmail.com>
2024-06-19 16:05:18 -0300

    forms

--- core/views.py
+++ core/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .forms import ContatoForm
 
 def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
 
 def produto(request):
     return render(request, 'produto.html')

62f450b192ec30cceb281ad55069015b5e9c7c39
Nycollas Henich <nhhenich@gmail.com>
2024-06-20 13:23:57 -0300

    Formulario, GET e POST

--- core/views.py
+++ core/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
 from .forms import ContatoForm
+from django.contrib import messages
 
 def index(request):
     return render(request, 'index.html')
 
 def contato(request):
     form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        print(f'Post: {request.POST}')
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print('Mensagem enviada')
+            print(f'Nome: {nome}')
+            print(f'Email: {email}')
+            print(f'Assunto: {assunto}')
+            print(f'Mensagem: {mensagem}')
+
+            messages.success(request, 'E-mail enviado com sucesso.')
+            form = ContatoForm()
+
+        else:
+            messages.error(request, 'Erro ao enviar.')
+
+
+
+
+
     context = {
         'form': form
     }

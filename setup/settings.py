361180d3b174aa209395f9d8b411106730654d90
Nycollas Henich <nhhenich@gmail.com>
2024-06-21 12:31:31 -0300

    models, forms, criacao dos produtos e email

--- setup/settings.py
+++ setup/settings.py
@@ -132,3 +132,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CONFIGURAÇÕES DE EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+"""
+EMAIL_HOST: 'localhost']
+EMAIL_HOST_USER: 'no-reply@seudominio.com.br
+EMAIL_PORT: 587
+EMAIL_USER_TSL: True
+EMAIL_HOST_PASSWORD: 'suasenha'
+"""
